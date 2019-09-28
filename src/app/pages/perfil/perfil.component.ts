7597236ad9cd15f450cc142c9943c78832c08316
Neryz <amneryshcpl25@gmail.com>
2019-09-28 05:08:36 -0500

    alerts
    puse validacion a noticias,usuario,agregar vacante y tambien alert

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -4,6 +4,7 @@ import {Router} from '@angular/router';
 import {UsuarioService} from 'src/app/services/usuarios.service';
 import {UsuarioModel} from 'src/app/models/usuario.model';
 import { FormControl } from '@angular/forms';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
@@ -21,13 +22,58 @@ export class PerfilComponent implements OnInit {
       this.usuarios = respuesta;
     });
   }
+
+  eliminarUsuarios(usuario: UsuarioModel){
+    Swal.fire({
+      title: '¿Está seguro de eliminar el usuario?',
+      text: "Una vez eliminado el usuario, no podrás recuperarlo",
+      type: 'warning',
+      showCancelButton: true,
+      cancelButtonColor: '#3085d6',
+      confirmButtonColor: '#d33 ',
+      confirmButtonText: 'Eliminar'
+    }) .then((result) =>  {
+
+      if (result.value) {
+        this.servicioUsuarios.eliminarUsuarios(usuario);
+        Swal.fire(
+          'Eliminado satisfactoriamente!',
+          'El usuario ha sido eliminado',
+          'success'
+        )
+      }
+    });
+  }
+  
   editarUsuarios (usuario: UsuarioModel){
     console.log('Voy a editar a:  ')
     this.servicioUsuarios.editarUsuarios( usuario );
+
+    Swal.fire({
+      title: 'Editado!',
+      text: 'Los cambios se guardaron correctamente',
+      type: 'success',
+      confirmButtonText: 'OK'
+    });
   }
+  
   verUsuario (usuario: any){
     this.usuario = usuario;
   }
+
+  
+  crearUsuario( usuario: UsuarioModel ){
+    this.servicioUsuarios.crearUsuario( usuario );
+    Swal.fire({
+      title: 'Agregado!',
+      text: 'El proceso se ha realizado con éxito',
+      type: 'success',
+      confirmButtonText: 'OK'
+    });
+  }
+
+ 
+
   ngOnInit() {
     this.getUsuarios();
   }
